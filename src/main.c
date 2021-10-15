5323762b7e6fc560efa45cd6e17d3294b82996f7
y.dechaux <y.dechaux@student.isartdigital.com>
2021-10-15 16:01:43 +0200

    premiere texture

--- src/main.c
+++ src/main.c
@@ -1,12 +1,19 @@
 #include <raylib.h>
-
+#include <mathematics.h>
 int main(void)
 {
-    const int screenWidth  = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 640;
+    const int screenHeight = 800;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
     SetTargetFPS(60);
+
+    Image background = LoadImage("assets/minestorm_background.png");
+    Image foreground = LoadImage("assets/minestorm_foreground.png");
+    Texture2D textback = LoadTextureFromImage(background);
+    Texture2D textfore = LoadTextureFromImage(foreground);
+    UnloadImage(background);
+    UnloadImage(foreground);
 
     // Main game loop
     while (!WindowShouldClose())
@@ -14,11 +21,13 @@ int main(void)
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawTexture(textback, 0, 0, WHITE);
+        DrawTexture(textfore, 0, 0, WHITE);
 
         EndDrawing();
     }
-
+    UnloadTexture(textback);
+    UnloadTexture(textfore);
     CloseWindow();
 
     return 0;
